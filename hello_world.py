94b476d67f481dc1e707f2063c1a0643f9704e11
Daniel Castaneda <daniel02castaneda@gmail.com>
2022-03-09 19:55:41 -0700

    pyhton

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+# TASK: print "Hello World"
+print('Hello World')
+#  TASK 2.
+name = 'Daniel'
+print('Hello', name)
+print('Hello ' + name)
+
+#  TASK 3: print  ' hello 42'
+name = 19
+print('Hello' , name)
+print('Hello ' + str(name))
+
+
+#  TASK 4: 
+
+fave_food1 = 'pizza'
+fave_food2 = 'pasta'
+
+print('I love to eat {} and {}'.format(fave_food1, fave_food2))
+print(f" I love to eat {fave_food1} and {fave_food2}")
